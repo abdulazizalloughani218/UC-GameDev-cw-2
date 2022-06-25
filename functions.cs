66ac9029ed9593aa6bad9bbf210747c7b0aa7616
abdulazizalloughani <108014988+abdulazizalloughani218@users.noreply.github.com>
2022-06-25 18:32:50 +0300

    functions

---
+++ functions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Functions_Cw
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double playerSpeed = 1 ;
+            double newplayerspeed;
+            double SetSpeed()
+            {
+                double additinalspeed = 2.5;
+                double newplayerSpeed = playerSpeed + additinalspeed;
+                Console.WriteLine("the old speed is " + playerSpeed + " and the new speed is " + newplayerSpeed);
+
+                return newplayerSpeed;
+
+            }
+            newplayerspeed = SetSpeed();
+
+            Console.WriteLine(newplayerspeed);
+        }
+    }
+}
