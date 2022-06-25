9813c5afd70882b2f45deb47aad17aaeb7e88878
abdulazizalloughani <108014988+abdulazizalloughani218@users.noreply.github.com>
2022-06-25 18:04:12 +0300

    if statement

---
+++ if statement.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace classwork_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string heroName = "Superman";
+            int heroPower = 20 ;
+
+            string villanName = "joker";
+            int villanPower = 25 ;
+
+            if (heroPower > villanPower)
+            {
+                Console.WriteLine( heroName + " is more powerful");
+
+            }
+            else if (heroPower < villanPower)
+            {
+                Console.WriteLine("The " + villanName+ " is more powerful ");
+            }
+            else
+            {
+                Console.WriteLine("the hero and the villan are equally powerful ");
+            }
+
+
+        }
+    }
+}
